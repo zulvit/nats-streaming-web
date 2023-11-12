0f21aaff5c1c8844cff53f6c934680418a4f55c4
savva <zulvit.sk@gmail.com>
2023-11-12 16:54:11 +0300

    Services and redis client add

--- cmd/main.go
+++ cmd/main.go
@@ -1,44 +1,33 @@
 package main
 
-import (
-	"context"
-	"nats-streaming-web/internal/logger"
-	"nats-streaming-web/pkg/client"
-	"nats-streaming-web/pkg/config"
-	"nats-streaming-web/pkg/model"
-	"nats-streaming-web/pkg/service"
-	"os"
-	"os/signal"
-	"syscall"
-)
-
 func main() {
-	// Загрузка конфигурации
-	cfg := config.NewConfig()
-
-	// Создание клиента NATS
-	natsClient, err := client.NewNatsClient(cfg)
-	if err != nil {
-		logger.ErrorLogger.Fatalf("Ошибка подключения к NATS: %v\n", err)
-	}
-	defer natsClient.Close()
-
-	// Создание публикатора
-	pub := service.NewPublisher(natsClient.Conn)
-
-	orderData := &model.OrderData{
-		OrderUID: "8f380f40",
-	}
-
-	// Публикация данных
-	err = pub.PublishOrder(context.Background(), orderData, cfg.Subject)
-	if err != nil {
-		logger.ErrorLogger.Printf("Ошибка при публикации данных: %v\n", err)
-	}
-
-	// Ожидание прерывания программы
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-	<-quit
-	logger.InfoLogger.Println("Завершение работы приложения")
+	//fixme сделать тут работу с web-сервисом
+	//// Загрузка конфигурации
+	//cfg := config.NewConfig()
+	//
+	//// Создание клиента NATS
+	//natsClient, err := client.NewNatsClient(cfg)
+	//if err != nil {
+	//	logger.ErrorLogger.Fatalf("Ошибка подключения к NATS: %v\n", err)
+	//}
+	//defer natsClient.Close()
+	//
+	//// Создание публикатора
+	//pub := service.NewPublisher(natsClient.Conn)
+	//
+	//orderData := &model.OrderData{
+	//	OrderUID: "8f380f40",
+	//}
+	//
+	//// Публикация данных
+	//err = pub.PublishOrder(context.Background(), orderData, cfg.Subject)
+	//if err != nil {
+	//	logger.ErrorLogger.Printf("Ошибка при публикации данных: %v\n", err)
+	//}
+	//
+	//// Ожидание прерывания программы
+	//quit := make(chan os.Signal, 1)
+	//signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	//<-quit
+	//logger.InfoLogger.Println("Завершение работы приложения")
 }
